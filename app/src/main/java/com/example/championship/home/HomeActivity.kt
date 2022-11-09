848c843ec0131d0ddc2a29175f4f80b50c8fed35
Watliel <arnaud.salomon20@gmail.com>
2022-11-09 11:47:40 +0100

    init project

---
+++ app/src/main/java/com/example/championship/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.championship.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.championship.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_main)
+    }
+}
---
+++ app/src/main/java/com/example/championship/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.championship.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+
+}
